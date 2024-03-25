13f0cda161400c34ed8015046bfac730f51bf9fd
Mithun Oorath <mithun.o@gmail.com>
2024-03-25 17:01:38 -0400

    initial commit

---
+++ src/main/java/com/mi/ai/solutions/documentdataextract/DocumentDataExtractApplication.java
@@ -0,0 +1,13 @@
+package com.mi.ai.solutions.documentdataextract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentDataExtractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentDataExtractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mi/ai/solutions/documentdataextract/DocumentDataExtractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mi.ai.solutions.documentdataextract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentDataExtractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
